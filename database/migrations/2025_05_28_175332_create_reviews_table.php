8af20511f7dc0c7e919ba4a962d38569089719c3
Vlad <vlad.zah@ukr.net>
2025-05-28 21:11:58 +0300

    BR-1 Added relationship

--- database/migrations/2025_05_28_175332_create_reviews_table.php
+++ database/migrations/2025_05_28_175332_create_reviews_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
             $table->text('review');
             $table->unsignedTinyInteger('rating');
             $table->timestamps();
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
         });
     }
 
